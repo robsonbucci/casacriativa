cf9ec30ba975ace6eac43d0134690d7c6979d9a9
robsonbucci <robsonbucci@hotmail.com>
2021-02-20 19:42:45 -0300

    vuejs

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function onOff() {
 
   document.querySelector("#modal").classList.toggle("addScroll");
 }
+
+new Vue({
+  el: '.descricao',
+  data: {
+    titulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  }
+})
