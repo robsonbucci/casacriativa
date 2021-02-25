60cbac35ccb56f2c4d630697e326558b06d472a2
robsonbucci <robsonbucci@hotmail.com>
2021-02-25 14:59:24 -0300

    v-for

--- script.js
+++ script.js
@@ -8,15 +8,58 @@ function onOff() {
 
 // framework Vuejs
 const sectionTitle = new Vue({
-  el: '#title',
+  el: '#page-ideas #title',
   data: {
     titulo: 'em exibição'
   }
 })
 
-const sectionDivDesc = new Vue({
-  el: '.descricao',
+new Vue({
+  el: '#page-ideas #ideas, #container #last-ideias',
   data: {
-    titulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    ideas: [
+      {
+        'img': 'https://www.flaticon.com/premium-icon/icons/svg/3242/3242257.svg',
+        'title': 'Cursos de Programação',
+        'category': 'Estudo',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'url': '#'
+      },
+      {
+        'img': "https://www.flaticon.com/premium-icon/icons/svg/3242/3242372.svg",
+        'title': "Exercícios",
+        'category': "Saúde",
+        'description': "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        'url': "#",
+      },
+      {
+        'img': "https://www.flaticon.com/premium-icon/icons/svg/3242/3242355.svg",
+        'title': "Ler Livros",
+        'category': "Raciocínio",
+        'description': "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        'url': "#",
+      },
+      {
+        'img': "https://www.flaticon.com/premium-icon/icons/svg/3242/3242389.svg",
+        'title': "Assistir Séries",
+        'category': "Séries",
+        'description': "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        'url': "#",
+      },
+      {
+        'img': "https://www.flaticon.com/premium-icon/icons/svg/3242/3242384.svg",
+        'title': "Fazer Novos Amigos",
+        'category': "Vida Social",
+        'description': "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        'url': "#",
+      },
+      {
+        'img': "https://www.flaticon.com/premium-icon/icons/svg/3242/3242322.svg",
+        'title': "Pintura",
+        'category': "Criatividade",
+        'description': "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        'url': "#",
+      },
+    ]
   }
 })
